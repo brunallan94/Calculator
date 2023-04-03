b54c73cefe586674d323787b48f7cb3b463212c1
brunallan94 <brunallan94@gmail.com>
2023-04-03 21:25:55 +0300

    second commit

--- main.py
+++ main.py
@@ -6,24 +6,27 @@ import logging
 logging.basicConfig(level = logging.DEBUG,
                     format = "%(asctime)s - %(levelname)s - %(message)s")
 
-def add(x, y):
+# creating functions that perform basic calculations
+def add(x = int, y = int):
     logging.info(f"{x} + {y} = {int(x)+int(y)}")
 
-def subtract(x, y):
+def subtract(x = int, y = int):
     logging.info(f"{x} - {y} = {int(x)-int(y)}")
     
-def multiply(x, y):
+def multiply(x = int, y = int):
     logging.info(f"{x} x {y} = {int(x)*int(y)}")
 
 def divide(x = int, y = int):
     logging.info(f"{x} / {y} = {int(x)/int(y)}")
 
+# Displaying information that shows the options available for user to choose from
 logging.info('WELCOME TO CALCULATOR \n')
 logging.info("""         A. addition
          B. subtraction
          C. multiplication
          D. division""")
 
+# Creating a while loop that collects the users data and performs the function given
 while True:
     answer = input('Select the choices from above or q to quit:')
     
